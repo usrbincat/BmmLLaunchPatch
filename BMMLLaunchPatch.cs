0b8734103146bc9766c14a0df6de179672f2e28d
/usr/bin/cat <4317605+usrbincat@users.noreply.github.com>
2022-01-06 17:03:27 -0600

    Update BMMLLaunchPatch.cs

--- BMMLLaunchPatch.cs
+++ BMMLLaunchPatch.cs
@@ -1,38 +1,21 @@
-﻿using BepInEx.Preloader.Core.Patching;
+using BepInEx.Preloader.Core.Patching;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using System.Collections.Generic;
 using System.Linq;
-//using Mono.Cecil;
-//using BepInEx;
-//using BepInEx.NetLauncher;
-//using System;
-//using BepInEx.Bootstrap;
-//using BepInEx.Configuration;
-//using BepInEx.Logging;
-
 
 [PatcherPluginInfo("BMMLLaunchPatch", "BmmL Launch Patch", "1.0.0")]
 public class BMMLLaunchPatch : BasePatcher
 {
 	[TargetAssembly("EMMA.Desktop.exe")]
-	public void PatchMain(AssemblyDefinition ass)
+	public void PatchMain(AssemblyDefinition asmDef)
 	{
-		//string n = "null";
-		//foreach (var x in ass.MainModule.Types) {
-		//	Console.WriteLine($"{x.Name} from {(x.BaseType != null ? x.BaseType.Name : n)}");
-		//}
-
 		// The method that needs to be patched is EMMA.Desktop.TheGame..ctor
-
-		TypeDefinition theGame = ass.MainModule.Types.First(
+		TypeDefinition theGame = asmDef.MainModule.Types.First(
 			x => x.Name == "TheGame" && x.BaseType != null && x.BaseType.Name == "TheGame");
 
-		//foreach (var m in theGame.Methods) {
-		//	Console.WriteLine(m.Name);
-		//}
-
 		MethodDefinition method = theGame.Methods.First(x => x.Name == ".ctor");
+
 
 		var il = method.Body.GetILProcessor();
 
@@ -42,12 +25,10 @@ public class BMMLLaunchPatch : BasePatcher
 			x.Next.OpCode == OpCodes.Ldc_I4 && 
 			(int)x.Next.Operand == 808090);
 
-		//Console.WriteLine(ins.OpCode.Name);
-
-		// Insert a return opcode before initialization of the steam Client can happen
+		// Insert a return opcode before initialization of the Client object can happen
 		il.InsertBefore(ins, il.Create(OpCodes.Ret));
 
-		// Remove all opcodes after the first return
+		// Remove all opcodes after the first return opcode
 		var ret = il.Body.Instructions.First(x => x.OpCode == OpCodes.Ret);
 
 		ins = il.Body.Instructions.Last();
@@ -58,42 +39,3 @@ public class BMMLLaunchPatch : BasePatcher
 		}
 	}
 }
-
-
-
-/*
-
-namespace proj
-{
-	public static class Patcher
-	{
-		// List of assemblies to patch
-		public static IEnumerable<string> TargetDLLs { get; } = new[] {"EMMA.Desktop.exe"};
-
-		// Patches the assemblies
-		public static void Patch(AssemblyDefinition assembly)
-		{
-			// Patcher code here
-			Console.WriteLine("test");
-		}
-	}
-}
-*/
-
-/*
-using BepInEx;
-using BepInEx.NetLauncher;
-
-namespace proj
-{
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BasePlugin
-    {
-        public override void Load()
-        {
-            // Plugin startup logic
-            Log.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-        }
-    }
-}
-*/
